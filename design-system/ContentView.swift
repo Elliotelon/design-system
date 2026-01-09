502150671d2326f3c73e467faedc54dcf311b13e
minkyu <elliotelon@gmail.com>
2026-01-09 16:30:31 +0900

    Initial Commit

---
+++ design-system/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  design-system
+//
+//  Created by 김민규 on 1/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ design-system/design_systemApp.swift
@@ -0,0 +1,17 @@
+//
+//  design_systemApp.swift
+//  design-system
+//
+//  Created by 김민규 on 1/9/26.
+//
+
+import SwiftUI
+
+@main
+struct design_systemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
